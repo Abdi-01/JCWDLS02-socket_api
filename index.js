24dbe203d4c94813e45bea4895fefe4735aacfdd
Abdi-01 <abdialghi@gmail.com>
2022-08-05 11:17:07 +0700

    socket

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const App = express();
+const PORT = 4000;
+const cors = require('cors');
+const http = require('http');
+const socketIO = require('socket.io');
+
+// Configure socket
+const httpServer = http.createServer(App);
+const io = socketIO(httpServer);
+
+App.use(cors());
+App.use(express.json());
+
+App.get('/', (req, res) => {
+    res.status(200).send('<h1>Socket API</h1>')
+});
+
+// Configure connection socket from client
+io.on('connection', (socket) => {
+    socket.on('JoinSocket', (data) => {
+        console.log("User join data :", data);
+        io.emit('joinNotif', `${data.username} Success Join Socket ✅`)
+    })
+})
+
+httpServer.listen(PORT, () => console.log(`Socket server : ${PORT}`));
